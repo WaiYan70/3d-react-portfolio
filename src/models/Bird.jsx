4de7a2b55d18e2a5b58fa5ae8c400a6df6e690a0
Khant Wai Yan <52145576+WaiYan70@users.noreply.github.com>
2024-03-01 14:23:15 +0630

    Seventh Commit, Fixed the the bird Rotation

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,13 +1,16 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useAnimations, useGLTF } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 
 import birdScene from '../assets/3d/bird.glb';
-import { useFrame } from '@react-three/fiber';
 
 const Bird = () => {
   const birdRef = useRef();
-  const {scene, animations} = useGLTF(birdScene);
-  const {actions} = useAnimations(animations, birdRef);
+  const { scene, animations } = useGLTF(birdScene);
+  const { actions } = useAnimations(animations, birdRef);
+
+  const targetRotation = useRef(0); // Target rotation for smooth transition
+  const rotationSpeed = 0.02; 
 
   useEffect(()=>{
     actions['Take 001'].play();
@@ -20,19 +23,31 @@ const Bird = () => {
     // if the bird reach a certain endpoint in the map, it will change the bird's direction
     if(birdRef.current.position.x > camera.position.x + 8){
       // change direction backward and rotate the bird 180 degree on y axis
-      birdRef.current.rotation.y = Math.PI;
+      // birdRef.current.rotation.y = Math.PI;
+      targetRotation.current = Math.PI;
     } else if(birdRef.current.position.x < camera.position.x - 8){
       // change direction forward and reset the bird's direction
-      birdRef.current.rotation.y = 0;
+      // birdRef.current.rotation.y = 0;
+      targetRotation.current = 0;
     }
 
+    // Smoothly interpolate rotation towards target rotation
+    birdRef.current.rotation.y += (targetRotation.current - birdRef.current.rotation.y) * rotationSpeed;
+
+    // if(birdRef.current.rotation.y === 0){
+    //   birdRef.current.position.x += 0.01;
+    //   birdRef.current.position.z -= 0.01;
+    // } else {
+    //   birdRef.current.position.x -= 0.01;
+    //   birdRef.current.position.z += 0.01;
+    // }
     // Update the X and Z positions based on the direction (Y position)
-    if(birdRef.current.rotation.y === 0){
-      // Moving forward
-      birdRef.current.position.x  += 0.01;
+    if(targetRotation.current === 0){
+      // Moving Forward
+      birdRef.current.position.x += 0.01;
       birdRef.current.position.z -= 0.01;
     } else {
-      //Moving backward
+      // Moving Backward
       birdRef.current.position.x -= 0.01;
       birdRef.current.position.z += 0.01;
     }
@@ -40,7 +55,7 @@ const Bird = () => {
 
   return (
     <mesh 
-      position={[ -5, 2, 1]} 
+      position={[ -3, 2, 1]} 
       scale={[0.003, 0.003, 0.003]} 
       ref={birdRef}>
       <primitive object={scene} />
